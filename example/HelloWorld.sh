d96f2dbbbd0ad800230683e5f2087d449233d32f
Aaron Stafford <aaron@staffordfarm.com.au>
2015-01-29 19:36:05 +1000

    Tweaked the example script to work better out of the box.

--- example/HelloWorld.sh
+++ example/HelloWorld.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-java -jar AZClassGenerator.jar --output-automaton
-#java -jar AZClassGenerator.jar --diagram example.scxml --class-name Example --file-type .h --file-type .cpp
+java -jar ../AZClassGenerator.jar --output-automaton
+java -jar ../AZClassGenerator.jar --diagram scxml/example.scxml --class-name Example --file-type .h --file-type .cpp
 
 g++ -c AZ.cpp
 g++ -c Automaton.cpp
