138da2443c93a6b7cc2fb5e811b8619fe56be148
Aaron Stafford <aaron@staffordfarm.com.au>
2016-03-26 16:36:36 +1300

    converted code over to use the newer style class definitions.

--- code/javascript/az.js
+++ code/javascript/az.js
@@ -1,115 +1,129 @@
 /*
  Begin definition of the hash table Entry class
 */
-function Entry(key, value)
+
+class Entry
 {
-  this.key = key;
-  this.value = value;
-}
+  constructor(key, value)
+  {
+    this.key = key;
+    this.value = value;
+  }
  
-Entry.prototype.getKey = function() {
-  return this.key;
-};
+  getKey()
+  {
+    return this.key;
+  }
 
-Entry.prototype.getValue = function() {
-  return this.value;
-};
+  getValue()
+  {
+    return this.value;
+  }
 
-Entry.prototype.setValue = function(value) {
-  var oldValue = this.value;
-  this.value = value;
-  return oldValue;
-};
+  setValue(value)
+  {
+    var oldValue = this.value;
+    this.value = value;
+    return oldValue;
+  }
+}
 
 
 /*
  Begin definition of the HashTable class
 */
-function HashTable(capacity)
+class HashTable
 {
-  this.capacity = capacity
-  this.table = [];
-  this.size = 0;
-  if(this.capacity == 0)
+  constructor(capacity)
   {
-    this.capacity = 1;
-  }
-  // Populate the array with empty arrays
-  for(var i = 0; i < capacity; i++)
-  {
-    this.table.push([]);
-  }
-};
-
-HashTable.prototype.getSize = function() {
-  return this.size;
-};
-
-HashTable.prototype.isEmpty = function() {
-  return size == 0;
-};
-
-HashTable.prototype.put = function(key, value)
-{
-  var index = (key & 0x7FFFFFFF) % this.capacity;
-  var entryArray = this.table[index];
-  for(var i = 0; i < entryArray.length; i++)
-  {
-    if(entryArray[i].key == key)
+    this.capacity = capacity
+    this.table = [];
+    this.size = 0;
+    if(this.capacity == 0)
     {
-      var old = entryArray[i].value;
-      entryArray[i].value = value;
-      return old;
+      this.capacity = 1;
+    }
+    // Populate the array with empty arrays
+    for(var i = 0; i < capacity; i++)
+    {
+      this.table.push([]);
     }
   }
-  // If we are here then there was no existing entry in the table
-  // Should check if re-hashing is required, but aren't.
-  this.table[index].push(new Entry(key, value));
-  this.size++;
-  return undefined;
+
+  getSize()
+  {
+    return this.size;
+  }
+
+  isEmpty()
+  {
+    return size == 0;
+  }
+
+  put(key, value)
+  {
+   var index = (key & 0x7FFFFFFF) % this.capacity;
+   var entryArray = this.table[index];
+   for(var i = 0; i < entryArray.length; i++)
+   {
+     if(entryArray[i].key == key)
+     {
+       var old = entryArray[i].value;
+       entryArray[i].value = value;
+       return old;
+     }
+   }
+   // If we are here then there was no existing entry in the table
+   // Should check if re-hashing is required, but aren't.
+   this.table[index].push(new Entry(key, value));
+   this.size++;
+   return undefined;
+  }
+
+  getValue(key)
+  {
+    var index = (key & 0x7FFFFFFF) % this.capacity;
+    var entryArray = this.table[index];
+    for(var i = 0; i < entryArray.length; i++)
+    {
+      if(entryArray[i].key == key)
+      {
+        return entryArray[i].value;
+      }
+    }
+    return undefined;
+  }
 }
-
-HashTable.prototype.get = function(key)
-{
-  var index = (key & 0x7FFFFFFF) % this.capacity;
-  var entryArray = this.table[index];
-  for(var i = 0; i < entryArray.length; i++)
-  {
-    if(entryArray[i].key == key)
-    {
-      return entryArray[i].value;
-    }
-  }
-  return undefined;
-};
 
 
 /*
  Begin definition of the State Machine class 
 */
-function StateMachine()
+class StateMachine
 {
-  var capacity = 11;
-  this.hashTable = new HashTable(capacity);
-};
+  constructor()
+  {
+    var capacity = 11;
+    this.hashTable = new HashTable(capacity);
+  }
 
-StateMachine.prototype.getNextState = function(objectID, currentState, input)
-{
-  var key = objectID << 20;
-  key |= currentState << 10;
-  key |= input;
-  var nextState = this.hashTable.get(key);
-  return nextState;
-};
+  getNextState(objectID, currentState, input)
+  {
+    var key = objectID << 20;
+    key |= currentState << 10;
+    key |= input;
+    var nextState = this.hashTable.getValue(key);
+    return nextState;
+  }
 
-StateMachine.prototype.insertTransition = function(objectID, currentState, input, newState)
-{
-  var key = objectID << 20;
-  key |= currentState << 10;
-  key |= input;
-  this.hashTable.put(key, newState);
-print("key value on insert = " + key);
-};
+  insertTransition(objectID, currentState, input, newState)
+  {
+    var key = objectID << 20;
+    key |= currentState << 10;
+    key |= input;
+    this.hashTable.put(key, newState);
+  }
+}
 
 var hashTable = new HashTable(1, 2);
 print(hashTable.put(1, 2));
@@ -117,8 +131,8 @@ print(hashTable.put(1, 3));
 print(hashTable.put(2, 5));
 
 print('get test');
-print(hashTable.get(1));
-print(hashTable.get(2));
+print(hashTable.getValue(1));
+print(hashTable.getValue(2));
 
 var stateMachine = new StateMachine();
 stateMachine.insertTransition(0, 0, 1);
