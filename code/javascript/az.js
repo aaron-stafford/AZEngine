c3b4b4a199242ccd4831ef8e93fe421cfed99b15
Aaron Stafford <aaron@staffordfarm.com.au>
2016-03-26 14:25:23 +1300

    renamed the file.

--- code/javascript/az.js
+++ code/javascript/az.js
@@ -16,13 +16,29 @@ Entry.prototype.getValue = function() {
 };
 
 Entry.prototype.setValue = function(value) {
+  var oldValue = this.value;
   this.value = value;
+  return oldValue;
 };
 
-var entry = new Entry(1, 2, 3, 4);
+var entry = new Entry(1, 2, "test", 4);
 print(entry.hash);
 print(entry.key);
 print(entry.getKey());
 print(entry.value);
 print(entry.getValue());
 print(entry.next);
+print(entry.setValue(5));
+print(entry.getValue());
+
+function MyEntry (hash, key, value, next)
+{
+  this.hash = hash;
+  this.key = key;
+  this.value = value;
+  this.next = next;
+}
+
+var myEntry = Object.create(new Entry(6, 7, "test2", 8));
+
+print(myEntry.getKey());
